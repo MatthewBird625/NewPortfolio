2e1459aa6e8f37bb0e19ccb77a45327e73e309dc
birdman769 <48811918+birdman769@users.noreply.github.com>
2022-11-03 18:22:49 +1100

    Updates

---
+++ precache-manifest.26fef56a84c21410378cc82a543fb7aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d5c39015feb8e80ca3e",
+    "url": "/NewPortfolio/static/css/main.f8fca8ec.chunk.css"
+  },
+  {
+    "revision": "2d5c39015feb8e80ca3e",
+    "url": "/NewPortfolio/static/js/main.2d5c3901.chunk.js"
+  },
+  {
+    "revision": "d5a82794cb252de57f41",
+    "url": "/NewPortfolio/static/js/1.d5a82794.chunk.js"
+  },
+  {
+    "revision": "9468d58d24420ab30f53",
+    "url": "/NewPortfolio/static/js/runtime~main.9468d58d.js"
+  },
+  {
+    "revision": "7108297234e6e486b8911e5927c1d2a5",
+    "url": "/NewPortfolio/index.html"
+  }
+];
