ea1e321b1b744e93f76b4392163221fc39d87c91
birdman769 <48811918+birdman769@users.noreply.github.com>
2022-11-03 21:55:22 +1100

    fixed bottom linkedIn link

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class ContactUs extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
       <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <p className="lead">
-              Feel free to contact me for any work or suggestions below
-              </p>
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">Feel free to contact me on LinkedIn</p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <a href="https://www.linkedin.com/in/matthew-bird-96363313a/">
+                LinkedIn
+              </a>
             </div>
-          </div>
-          <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
-                </h4>
-              </div>
-            </aside>
-          </div>
-        </section>
-        );
+          </aside>
+        </div>
+      </section>
+    );
   }
 }
