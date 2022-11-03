1f01a9f3c215bf9ad38f4f9aa7b0e8e8538e6e1e
birdman769 <48811918+birdman769@users.noreply.github.com>
2022-11-03 18:21:44 +1100

    created a portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,94 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Matthew Bird",
+  role: "Graduate Developer",
+  linkedinId: "linkedin.com/in/matthew-bird-96363313a",
+  skypeid: "",
+  roleDescription:
+    " I am open to various fields of development for my Graduate position. I have experience in AWS, Terraform, Anisble, CI/ CD, Java, Javascript, React, C# and more!",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/matthew-bird-96363313a/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/MatthewBird625",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    " I am currently a final year IT student who has completed all my classes and is ready to Graduate, with distinction on the 14th of December. Outside of coding and my Studies I like to attend Gym and cook!",
+  address: "Melbourne- Australia",
+  website: "https://github.com/MatthewBird625",
+  education: [
+    {
+      UniversityName: "RMIT University- Melbourne",
+      specialization: "IT- Open Major",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2022",
+      Achievements: "Graudating with distinction",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Carnival Guide",
+      specialization: "Front End Developer- continuing internship",
+      MonthOfLeaving: "July",
+      YearOfLeaving: "2022 - current",
+      Achievements:
+        "A continuing position in with my client from my industry project beyond the scope of Uni.In this project I managed a team of four students as the project manager. I oversaw the development of a React project as well as performed my own React coding tasks. I adminstrated all JIRA tasks and stories and handled all client negotations. Furthermore I also performed the management of the teams GitHub and performed the role of the Technical Lead.",
+    },
+    {
+      CompanyName: "Power-Tec",
+      specialization: "Student software developer",
+      MonthOfLeaving: "Nov 2019 -",
+      YearOfLeaving: "Sep 2022",
+      Achievements:
+        "Performed a range of coding tasks on two projects that ran in parallel to assist engineers and software developers with the rapid development of two BESS systems prototypes. I used a range of tools and software including: mongoDB, HTML,CSS, Javascript and Angular. Contact for .pdf resume for more details. ",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "Java",
+    },
+    {
+      skillname: "Javascript/ HTML / CSS",
+    },
+    {
+      skillname: "React / Typescript",
+    },
+    {
+      skillname: "C#",
+    },
+    {
+      skillname: "Oracle / MongoDB / MySQL",
+    },
+    {
+      skillname: "AWS and Infrastructure",
+    },
+    {
+      skillname: "CI/ CD",
+    },
+    {
+      skillname: "Terraform / Ansible",
+    },
+    {
+      skillname: "Linux/ Headless",
+    },
+    {
+      skillname: "Scrum / Agile methods",
+    },
+    {
+      skillname: "Project Management Skills",
+    },
+
+    {
+      skillname: "Github and GitHub Actions",
+    },
+  ],
+};
+
+export default resumeData;
